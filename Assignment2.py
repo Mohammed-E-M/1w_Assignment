ede68d22490f7920fb36f92f36bd58ebda5116b1
Mohammed-E-M <m.e.makhloufi@outlook.com>
2025-05-16 12:56:42 +0100

    Update Assignment2.py

--- Assignment2.py
+++ Assignment2.py
@@ -1,20 +1,23 @@
 import random
 
-
-input("Press Enter to start the game")
+# This part is to ensure the user is engaged
+input("Press Any key to start the game")
+#Random generator(1-50)
 number = random.randint(1, 50)
 
 
 while True:
    guess = int(input("Guess a number between 1 and 50: "))
-    if not guess_input.isdigit():
+   #Ensure that only numbers are allowed to be entered
+   if not guess_input.isdigit():
             print("Invalid input. Please enter a number.")
             continue
-
+#convert the guess to  an integer
         guess = int(guess_input)
    if guess == number:
        print("You guessed it right!")
        break
+      #Loop while giving hints until the correct guess
    elif guess < number:
        print("Too low, try again!")
    else:
